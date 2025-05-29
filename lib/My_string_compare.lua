e58b33bd2f474f781dcadd62ab407d68997b28b2
Adrien Gregoire <adrien.gregoire@epitech.eu>
2025-05-29 13:01:25 +0200

    feat(my_string_compare.lua): compare two strings

---
+++ lib/My_string_compare.lua
@@ -0,0 +1,17 @@
+--[[
+-- SOLO PROJECT, 2025
+-- My_string_compare
+-- File description:
+-- compare two strings
+]]
+
+function My_string_compare(a, b)
+    if a == b then
+        return 0;
+    else if a < b or a > b then
+        return -1;
+    else
+        return 1;
+        end;
+    end;
+end;
