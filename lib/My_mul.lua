7cfad26514b9ca7a93cce89707c93b8b852994a2
Adrien Gregoire <adrien.gregoire@epitech.eu>
2025-05-29 14:06:28 +0200

    feat(My_mul.lua): multiplie two numbers

---
+++ lib/My_mul.lua
@@ -0,0 +1,12 @@
+--[[
+-- SOLO PROJECT, 2025
+-- My_mul
+-- File description:
+-- multiplie two numbers
+]]
+
+function My_mul(x, y)
+    local number = x * y;
+    print(number);
+    return 0;
+end;
